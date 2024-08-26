8e2f99ec48941ffc2c299a7fd6f9d07cd2453f87
Naeem Muhammad Akram <85845054+naeem92@users.noreply.github.com>
2024-08-27 04:07:13 +0500

    Update AddProduct.jsx

--- admin/src/Components/Navbar/AddProduct/AddProduct.jsx
+++ admin/src/Components/Navbar/AddProduct/AddProduct.jsx
@@ -30,7 +30,7 @@ const AddProduct = () => {
     let formData = new FormData();
     formData.append('product',image);
 
-    await fetch('http://localhost:4000/upload',{
+    await fetch('https://shopper-backend-tan.vercel.app/upload',{
         method: 'POST',
         headers:{
             Accept:'application/json',
@@ -42,7 +42,7 @@ const AddProduct = () => {
         {
             product.image = responseData.image_url;
             // console.log(product)
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://shopper-backend-tan.vercel.app/addproduct',{
                 method: 'POST',
                 headers:{
                     Accept:'application/json',
