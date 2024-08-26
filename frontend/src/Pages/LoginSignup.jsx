4da6720ff4d4b1783b69d154dae1ff9b55fde8da
Naeem Muhammad Akram <85845054+naeem92@users.noreply.github.com>
2024-08-27 04:06:13 +0500

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -16,7 +16,7 @@ const LoginSignup = () => {
   const login = async ()=>{
     // console.log("login",formData)
     let responseData;
-    await fetch('http://localhost:4000/login',{
+    await fetch('https://shopper-backend-tan.vercel.app/login',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
@@ -38,7 +38,7 @@ const LoginSignup = () => {
   const signup = async ()=>{
     // console.log("signup",formData)
     let responseData;
-    await fetch('http://localhost:4000/signup',{
+    await fetch('https://shopper-backend-tan.vercel.app/signup',{
       method:'POST',
       headers:{
         Accept:'application/form-data',
